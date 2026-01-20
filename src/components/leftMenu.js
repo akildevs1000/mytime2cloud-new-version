631834bf8e4ca38e68c467c1460692714dcd749e
akildevs1000 <akildevs1000@gmail.com>
2026-01-20 11:36:22 +0400

    test

--- src/components/leftMenu.js
+++ src/components/leftMenu.js
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
 import { leftNavLinks } from '../lib/menuData';
-import { LogInIcon } from "lucide-react";
+import { LogOutIcon } from "lucide-react";
 
 export default function LeftMenu() {
   const pathname = usePathname();
@@ -46,34 +46,34 @@ export default function LeftMenu() {
               >
                 {link.label}
               </span>
-
-
-
-
             </Link>
           );
         })}
       </nav>
 
       {/* Bottom section */}
-      <div className="mt-auto flex items-center justify-center group-hover:justify-start w-full px-0 group-hover:px-4 mb-2 transition-all duration-300">
-        <a
-          className="flex items-center w-14 group-hover:w-full py-3 rounded-lg text-gray-300 hover:bg-gray-700 hover:text-white 
-                     transition-all duration-200"
-          href="#"
+
+      <Link
+        href="#"
+        className={`ml-3 flex items-center w-14 group-hover:w-[90%] rounded-xl px-0 group-hover:px-4 py-3 
+                           transition-all duration-300 ease-in-out text-gray-500 dark:text-gray-100 hover:bg-gray-700 hover:text-white
+             `}
+      >
+        {/* Icon container stays centered in collapsed state */}
+        <div className="flex justify-center w-full group-hover:w-8 group-hover:justify-start transition-all duration-300">
+          <LogOutIcon size={22} strokeWidth={1.8} />
+        </div>
+
+        {/* Label only appears when hovered */}
+        <span
+          className="overflow-hidden w-0 opacity-0 group-hover:w-auto group-hover:opacity-100 
+             transition-all duration-300 whitespace-nowrap text-sm font-medium ml-0 group-hover:ml-2"
         >
-          <div className="flex justify-center w-full group-hover:w-8 group-hover:justify-start transition-all duration-300">
-            <LogInIcon size={22} strokeWidth={1.8} />
-          </div>
-          <span
-            className="ml-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0 
-                       transform -translate-x-2 transition-all duration-300 ease-in-out 
-                       whitespace-nowrap text-sm font-medium"
-          >
-            Login
-          </span>
-        </a>
-      </div>
+          LogOut
+        </span>
+      </Link>
+
+
     </aside>
   );
 }
